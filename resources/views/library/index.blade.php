ed8c02a53613b8925b974bea1e01d89240434020
semirkadric <71122720+semirkadric@users.noreply.github.com>
2021-10-20 14:32:36 +0200

    Informatie songs,books,actors,films compleet gemaakt

--- resources/views/library/index.blade.php
+++ resources/views/library/index.blade.php
@@ -1,7 +1,64 @@
 @extends('layout')
 
 @section('content')
-    <div class="">
-        <h1>hallo</h1>
-    </div>
+
+        <div class="container-themas">
+
+            <div class="songs">
+
+                <h1>Songs</h1>
+
+                <img src="" alt="">
+
+                <a href="">Bohemian Rapshody</a>
+
+                <a href="">Madness</a>
+
+                <a href="">Uprising</a>
+
+                <a href="">Plug in baby</a>
+
+                <a href="">Transylvia</a>
+
+                <a href="">We are the champions</a>
+
+            </div>
+
+            <div class="films">
+                <h1>Films</h1>
+
+                <img src="" alt="">
+
+                <a href="">James Bond Films</a>
+            </div>
+
+            <div class="film-personages">
+
+                <h1>Actors</h1>
+
+                <img src="" alt="">
+
+                <a href="">George Clooney</a>
+                
+                <a href="">Darth Vader</a>
+
+                <a href="">Timoty Dalton</a>
+
+                <a href="">Luke Skywalker</a>
+
+
+            </div>
+
+            <div class="books">
+
+                <h1>Books</h1>
+
+                <img src="" alt="">
+
+                <a href=""></a>
+
+            </div>
+
+        </div>
+
 @endsection
