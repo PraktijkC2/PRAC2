87ce152997bca40f712a787396bffb0ac9d759aa
omerovicmehmed <71382890+omerovicmehmed@users.noreply.github.com>
2021-10-20 14:02:39 +0200

    Footer links aangepast
    Footer beschikt nu over werkende links + aanpassing icoon.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,9 +31,9 @@ date_default_timezone_set('Europe/Amsterdam');
     <h1>CodeCamp &copy; 2021</h1>
     <h1>Koestraat 1, 's-Hertogenbosch</h1>
     <div class="footer-contact-links">
-        <a href=""><i class="fas fa-envelope-square"></i> Email</a>
-        <a href=""><i class="fab fa-twitter-square"></i> Twitter</a>
-        <a href=""><i class="fas fa-hashtag"></i> Instagram</a>
-        <a href=""><i class="fas fa-phone-square-alt"></i> 076 4812 28</a>
+        <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjgmIX29tjzAhWLCOwKHa0ZCmgQFnoECAsQAQ&url=https%3A%2F%2Fwww.google.com%2Fgmail%2F&usg=AOvVaw3mZ_qbD_gQyp_sqkjrwStn"><i class="fas fa-envelope-square"></i> Email</a>
+        <a href="https://twitter.com/"><i class="fab fa-twitter-square"></i> Twitter</a>
+        <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></i> Instagram</a>
+        <a href=""><i class="fas fa-phone-square-alt"></i> 073 48 12 28</a>
     </div>
 @endsection
