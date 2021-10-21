a21d6b86cd5cf5567a0d29b8c4f8819d482d6f38
semirkadric <71122720+semirkadric@users.noreply.github.com>
2021-10-21 08:30:22 +0200

    note picture toegevoegd aan songs

--- resources/views/library/index.blade.php
+++ resources/views/library/index.blade.php
@@ -4,13 +4,14 @@
 
         <div class="container-themas">
             <div class="songs">
-
                 <h1>Songs</h1>
-                <img src="" alt="">
+                
+            <div class="song-links">
+                <img src="img/note.png" alt="">
 
                 <a href="">Bohemian Rapshody</a>
 
-                <a href="">Madness</a>
+                <a href="">Madness &gt; </a>
 
                 <a href="">Uprising</a>
 
@@ -19,11 +20,11 @@
                 <a href="">Transylvia</a>
 
                 <a href="">We are the champions</a>
+                </div>
             </div>
 
             <div class="films">
                 <h1>Films</h1>
-
                 <img src="" alt="">
 
                 <a href="">James Bond Films</a>
