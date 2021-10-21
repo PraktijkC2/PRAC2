c4f11a2f31591e946febaccb6226ee819a466236
semirkadric <71122720+semirkadric@users.noreply.github.com>
2021-10-21 10:46:48 +0200

    nieuwe design aangemaakt en css toegevoegd

--- resources/views/library/index.blade.php
+++ resources/views/library/index.blade.php
@@ -2,61 +2,41 @@
 
 @section('content')
 
-        <div class="container-themas">
-            <div class="songs">
-                <h1>Songs</h1>
-                
-            <div class="song-links">
-                <img src="img/note.png" alt="">
+<div class="container">
+		<div class="card">
+			<div class="box">
+				<div class="content">
+				<h2>Songs</h2>
+				<p> Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Numquam cumque rerum saepe est similique aspernatur fugit ab repudiandae aperiam. </p>
+				<a href="#"> Read More </a>
+		 </div>
+	   </div>
+	</div> 	
+	<div class="card">
+			<div class="box">
+				<div class="content">
+				<h2>Films</h2>
+				<p> Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Numquam cumque rerum saepe est similique aspernatur fugit ab repudiandae aperiam. </p>
+				<a href="#"> Read More </a>
+		 </div>
+	   </div>
+	</div>
+	<div class="card">
+			<div class="box">
+				<div class="content">
+				<h2>Actors</h2>
+				<p> Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Numquam cumque rerum saepe est similique aspernatur fugit ab repudiandae aperiam. </p>
+				<a href="#"> Read More </a>
+		 </div>
+	   </div>
+	</div>
+    <div class="card">
+			<div class="box">
+				<div class="content">
+				<h2>Books</h2>
+				<p> Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Numquam cumque rerum saepe est similique aspernatur fugit ab repudiandae aperiam. </p>
+				<a href="#"> Read More </a>
+  </div>
 
-                <a href="">Bohemian Rapshody</a>
-
-                <a href="">Madness &gt; </a>
-
-                <a href="">Uprising</a>
-
-                <a href="">Plug in baby</a>
-
-                <a href="">Transylvia</a>
-
-                <a href="">We are the champions</a>
-                </div>
-            </div>
-
-            <div class="films">
-                <h1>Films</h1>
-                <img src="" alt="">
-
-                <a href="">James Bond Films</a>
-            </div>
-
-            <div class="actors">
-
-                <h1>Actors</h1>
-
-                <img src="" alt="">
-
-                <a href="">George Clooney</a>
-                
-                <a href="">Darth Vader</a>
-
-                <a href="">Timoty Dalton</a>
-
-                <a href="">Luke Skywalker</a>
-
-
-            </div>
-
-            <div class="books">
-
-                <h1>Books</h1>
-
-                <img src="" alt="">
-
-                <a href=""></a>
-
-            </div>
-
-        </div>
 
 @endsection
