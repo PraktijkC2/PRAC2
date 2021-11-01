9de2371120d0422511fcb9413b74a32a600a236d
semirkadric <71122720+semirkadric@users.noreply.github.com>
2021-11-01 12:27:52 +0100

    My library nieuwe style en informatie ingezet

--- resources/views/library/index.blade.php
+++ resources/views/library/index.blade.php
@@ -1,49 +1,34 @@
 @extends('layout')
 
 @section('content')
+<div class="container height-fix">
+    <div class="boxe cyan">
+        <h2>Songs</h2>
+        <a href="#">Bohemian Rhapsody</a>
+        <a href="#">Madness</a>
+        <a href="#">Uprising</a>
+        <a href="#">Plug in Baby</a>
+        <a href="#">We are the champions!</a>
+    </div>
 
-<div class="container">
-		<div class="card">
-			<div class="box">
-				<div class="content">
-				<h2>Songs</h2>
-                <a href="">Bohemian Rhapsody</a>
-                <a href="">Madness</a>
-                <a href="">Uprising</a>
-                <a href="">Plug in baby</a>
-                <a href="">Transylvania</a>
-                <a href="">We are the champions</a>
-            </div>
-	   </div>
-	</div> 	
-	<div class="card">
-			<div class="box">
-				<div class="content">
-				<h2>Films</h2>
-				<a href="">James Bond</a>
-                <a href="">The Pursuit Of Happyness</a>
-		 </div>
-	   </div>
-	</div>
-	<div class="card">
-			<div class="box">
-				<div class="content">
-				<h2>Actors</h2>
-				<a href="">George Clooney</a>
-                <a href="">Darth Vader</a>
-                <a href="">Timoty Dalton</a>
-                <a href="">Luke Skywalker</a>
-		 </div>
-	   </div>
-	</div>
-    <div class="card">
-			<div class="box">
-				<div class="content">
-				<h2>Books</h2>
-				<a href="">Undisputed Truth</a>
-                <a href="">Harry Potter</a>
-                <a href="">In Search of Lost Time</a>    
-  </div>
+    <div class="boxe red">
+        <h2>Films</h2>
+        <a href="#">James Bond</a> 
+    </div>
 
+    <div class="boxe blue">
+        <h2>Actors</h2>
+        <a href="#" target="_blank">George Clooney</a> 
+        <a href="#" target="_blank">Darth Vader</a> 
+        <a href="#" target="_blank">Timoty Dalton</a> 
+        <a href="#" target="_blank">Luke Skywalker</a> 
 
+    </div>
+	<div class="boxe purple">
+        <h2>Books</h2>
+        <a href="#">The Greatest</a> 
+        <a href="#">Undisputed Truth</a>
+    </div>
+
+</div>
 @endsection
