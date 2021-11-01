5be18e173eea0d5440df9147c7f1d337b0cff6f7
Ilyas I. Mohamed <d206947@edu.rocwb.nl>
2021-11-01 11:59:21 +0100

    #20 links aangemaakt in stakeholder
    In stakeholder pagina kaarten aangemaakt extern website, gereedschap en dranken

--- resources/views/stakeholders/index.blade.php
+++ resources/views/stakeholders/index.blade.php
@@ -1,7 +1,32 @@
 @extends('layout')
 
 @section('content')
-    <div class="">
-        <h1>hallo</h1>
+<div class="container height-fix">
+    <div class="boxe cyan">
+        <h2>Gereedschap</h2>
+        <a href="#">Cirkelzaag</a>
+        <a href="#">Kettingzaag</a>
+        <a href="#">Hamer</a>
+        <a href="#">Beitel</a>
+        <a href="#">Schroevendraaiers</a>
+        <a href="#">Handzagen</a>
     </div>
+
+    <div class="boxe red">
+        <h2>Dranken</h2>
+        <a href="#">Frisdrank</a> 
+        <a href="#">Warme dranken</a>
+    </div>
+
+    <div class="boxe blue">
+        <h2>Extern website</h2>
+        <a href="https://github.com/PraktijkC2/PRAC2" target="_blank">Code referenrences</a> 
+        <a href="https://github.com/PraktijkC2/PRAC2/projects/1" target="_blank">Kanban board</a> 
+        <a href="https://www.imdb.com/" target="_blank">IMDB</a> 
+        <a href="https://www.videoland.com/nl/" target="_blank">VideoLand</a> 
+        <a href="https://www.themovies.nl/" target="_blank">The Movies</a> 
+
+    </div>
+
+</div>
 @endsection
