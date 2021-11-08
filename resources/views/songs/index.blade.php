3125eeeeadb09b48e92bf74ded0db331565fb0fe
YasinOkutan <71122436+YasinOkutan@users.noreply.github.com>
2021-11-08 14:09:58 +0100

    fix #6 #7 #8 #9 #11
    Songteskt pagina aangemaakt en gestyled

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -1,9 +1,18 @@
 @extends('layout')
 
 @section('content')
-    <div class="">
-        {{ $songs->songname }}
-        {{ $songs->artiest }}
-        {{ $songs->songtekst }}
-    </div>
+<body>
+
+    <body style="background-color:{{ $songs->achtergrond }};">
+        <div class="wrapper">
+            <div class="song-info">
+                <h3 class="song-title">{{ $songs->songname }}</h3>
+                <p class="artist">{{ $songs->artiest }}</p>
+                <div style="color: {{ $songs->tekstkleur }}" class="songtekst">
+                    <p style="line-height:25px;">{{ $songs->songtekst }}</p>
+                </div>
+                <a href="{{ $songs->url }}">Meer Info</a>
+            </div>
+        </div>
+    </body>
 @endsection
