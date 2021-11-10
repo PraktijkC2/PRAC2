f89cb8518d5d4a2a1853880c59a160624cb3f4e8
YasinOkutan <71122436+YasinOkutan@users.noreply.github.com>
2021-11-10 14:21:29 +0100

    fix #26
    Actors pagina aangemaakt en gestyled

--- resources/views/actors/index.blade.php
+++ resources/views/actors/index.blade.php
@@ -1,8 +1,17 @@
 @extends('layout')
 
 @section('content')
-    <div class="">
-        {{ $actors->voornaam }} {{ $actors->achternaam }}
-        {{ $actors->info }}
+
+    <div class="wrapper">
+        <div class="actor-container">
+            <div class="actor">
+                <img src="{{ $actors->image }}" alt="Actors image">
+                <p>{{ $actors->voornaam }} {{ $actors->achternaam }}</p>
+            </div>
+            <div class="actor-info">
+                <p>{{ $actors->info }}</p>
+                <a href="{{ $actors->url }}">Meer info</a>
+            </div>
+        </div>
     </div>
 @endsection
