18bae1267e985642b58b4b688342dbffd718460f
omerovicmehmed <71382890+omerovicmehmed@users.noreply.github.com>
2021-11-10 14:25:06 +0100

    Gereedschappen subgroep

--- resources/views/stakeholders/index.blade.php
+++ resources/views/stakeholders/index.blade.php
@@ -6,26 +6,33 @@
     <div class="box cyan">
         <h2>Gereedschap</h2>
         @foreach($gereedschap as $grdschap)
-            <a href="{{ route('gereedschap.index', $grdschap->id) }}">{{ $grdschap->name }}</a>
+            @if(strpos($grdschap, 'none') !== false)
+                <a href="{{ route('gereedschap.index', $grdschap->id) }}">{{ $grdschap->name }}</a>
+            @endif
+        @endforeach
+        
+        <h4>Zagen</h4>
+        @foreach($gereedschap as $grdschap)
+            @if(strpos($grdschap, 'zagen') !== false)
+                <a href="{{ route('gereedschap.index', $grdschap->id) }}">{{ $grdschap->name }}</a>
+            @endif
         @endforeach 
     </div>
 
     <div class="box red">
         <h2>Dranken</h2>
-        
-        <h4>Frisdranken</h4>
+
         @foreach($dranken as $drank)
             @if(strpos($drank, 'frisdranken') !== false)
-                <a href="{{ route('dranken.index', $drank->categorie) }}">{{ $drank->name }}</a>
+                <a href="{{ route('dranken.index', $drank->id) }}">{{ $drank->name }}</a>
             @endif
-        @endforeach 
+        @endforeach
 
-        <h4>Warmedranken</h4>
         @foreach($dranken as $drank)
             @if(strpos($drank, 'warmedranken') !== false)
-                <a href="{{ route('dranken.index', $drank->categorie) }}">{{ $drank->name }}</a>
+                <a href="{{ route('dranken.index', $drank->id) }}">{{ $drank->name }}</a>
             @endif
-        @endforeach 
+        @endforeach
     </div>
 </div>
 @endsection
