73a97ba7fdecc998cb0bede64115623f4fbf62d5
YasinOkutan <71122436+YasinOkutan@users.noreply.github.com>
2021-11-10 15:22:47 +0100

    like button in actors en songs pagine
    like button toegevoegd op actors en songs pagina

--- resources/views/actors/index.blade.php
+++ resources/views/actors/index.blade.php
@@ -10,8 +10,25 @@
             </div>
             <div class="actor-info">
                 <p>{{ $actors->info }}</p>
-                <a href="{{ $actors->url }}">Meer info</a>
+                <div class="actor-actions">
+                    <a href="{{ $actors->url }}">Meer info</a>
+                    <Button class="like" onclick ="Toggle1()" id="btnh1" class="btn-like"><i class="fas fa-heart"></i></Button>
+                </div>
             </div>
         </div>
     </div>
+
+    <script>
+
+        var btnvar1 = document.getElementById('btnh1');
+
+        function Toggle1(){
+            if(btnvar1.style.color =="red"){
+             btnvar1.style.color = "grey"
+            }
+            else{
+             btnvar1.style.color ="red"
+            }
+        }
+    </script>
 @endsection
--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -1,16 +1,36 @@
 @extends('layout')
 
 @section('content')
-<body style="background-color:{{ $songs->achtergrond }};">
-    <div class="wrapper">
-        <div class="song-info">
-            <h3 class="song-title">{{ $songs->songname }}</h3>
-            <p class="artist">{{ $songs->artiest }}</p>
-            <div style="color: {{ $songs->textcolor }}" class="songtekst">
-                <p style="line-height:25px;">{{ $songs->songtekst }}</p>
+    <body style="background-color:{{ $songs->achtergrond }};">
+        <div class="wrapper">
+            <div class="song-info">
+                <h3 class="song-title">{{ $songs->songname }}</h3>
+                <p class="artist">{{ $songs->artiest }}</p>
+                <div style="color: {{ $songs->tekstkleur }}" class="songtekst">
+                    <p style="line-height:25px;">{{ $songs->songtekst }}</p>
+                </div>
+                <div class="songs-actions">
+                    <a href="{{ $songs->url }}">Meer Info</a>
+                    <Button class="like" onclick ="Toggle1()" id="btnh1" class="btn-like"><i class="fas fa-heart"></i></Button>
+                </div>
             </div>
-            <a href="{{ $songs->url }}">Meer Info</a>
         </div>
-    </div>
-</body>
+    </body>
+
+    <script>
+
+        var btnvar1 = document.getElementById('btnh1');
+
+        function Toggle1(){
+            if(btnvar1.style.color =="red"){
+            btnvar1.style.color = "grey"
+            }
+            else{
+            btnvar1.style.color ="red"
+            }
+        }
+    </script>
+
+
+
 @endsection
