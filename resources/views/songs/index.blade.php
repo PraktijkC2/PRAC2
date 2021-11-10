831918bead00c05a1d734ca9540193d2d8988e7f
meeelm <melmhq377@gmail.com>
2021-11-10 14:08:40 +0100

    Updated categorie pages & bugfixes

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -1,18 +1,16 @@
 @extends('layout')
 
 @section('content')
-<body>
-
-    <body style="background-color:{{ $songs->achtergrond }};">
-        <div class="wrapper">
-            <div class="song-info">
-                <h3 class="song-title">{{ $songs->songname }}</h3>
-                <p class="artist">{{ $songs->artiest }}</p>
-                <div style="color: {{ $songs->tekstkleur }}" class="songtekst">
-                    <p style="line-height:25px;">{{ $songs->songtekst }}</p>
-                </div>
-                <a href="{{ $songs->url }}">Meer Info</a>
+<body style="background-color:{{ $songs->achtergrond }};">
+    <div class="wrapper">
+        <div class="song-info">
+            <h3 class="song-title">{{ $songs->songname }}</h3>
+            <p class="artist">{{ $songs->artiest }}</p>
+            <div style="color: {{ $songs->tekstkleur }}" class="songtekst">
+                <p style="line-height:25px;">{{ $songs->songtekst }}</p>
             </div>
+            <a href="{{ $songs->url }}">Meer Info</a>
         </div>
-    </body>
+    </div>
+</body>
 @endsection
