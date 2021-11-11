249a76ca2473ef4de653b9fc0c2ffaebdedac7f0
Ilyas I. Mohamed <d206947@edu.rocwb.nl>
2021-11-11 11:21:36 +0100

    Fix #43, Fix#46
    Toegevoegd script van like icon van films, songs en boeken.

--- resources/views/films/index.blade.php
+++ resources/views/films/index.blade.php
@@ -25,4 +25,15 @@
             </div>
         </div>
     </div>
+    <script>
+        var btnvar1 = document.getElementById('btnh1'); 
+        function Toggle1(){
+            if(btnvar1.style.color =="red"){
+                btnvar1.style.color = "grey"
+            }
+            else{
+                btnvar1.style.color ="red"
+            }
+        }
+    </script>
 @endsection
