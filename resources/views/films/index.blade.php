94dcbc69011bd71944762a1b0bbaf8f2b382d936
Ilyas I. Mohamed <d206947@edu.rocwb.nl>
2021-11-11 11:22:02 +0100

    Revert "Fix #43, Fix#46"
    This reverts commit 249a76ca2473ef4de653b9fc0c2ffaebdedac7f0.

--- resources/views/films/index.blade.php
+++ resources/views/films/index.blade.php
@@ -25,15 +25,4 @@
             </div>
         </div>
     </div>
-    <script>
-        var btnvar1 = document.getElementById('btnh1'); 
-        function Toggle1(){
-            if(btnvar1.style.color =="red"){
-                btnvar1.style.color = "grey"
-            }
-            else{
-                btnvar1.style.color ="red"
-            }
-        }
-    </script>
 @endsection
