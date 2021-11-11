0144bb7320d815e169ee0f99a4d3dcf83eff3aa4
meeelm <melmhq377@gmail.com>
2021-11-11 10:17:58 +0100

    added condition for max-allowed categories for display

--- resources/views/stakeholders/index.blade.php
+++ resources/views/stakeholders/index.blade.php
@@ -10,7 +10,7 @@
                 <a href="{{ route('gereedschap.index', $grdschap->id) }}">{{ $grdschap->name }}</a>
             @endif
         @endforeach
-        
+        <br>
         <h4>Zagen</h4>
         @foreach($gereedschap as $grdschap)
             @if(strpos($grdschap, 'zagen') !== false)
@@ -22,13 +22,13 @@
     <div class="box red">
         <h2>Dranken</h2>
 
-        @foreach($dranken as $drank)
+        @foreach($dranken->take(1) as $drank)
             @if(strpos($drank, 'frisdranken') !== false)
                 <a href="{{ route('dranken.index', $drank->categorie) }}">{{ $drank->categorie }}</a>
             @endif
         @endforeach
 
-        @foreach($dranken as $drank)
+        @foreach($dranken->take(2) as $drank)
             @if(strpos($drank, 'warmedranken') !== false)
                 <a href="{{ route('dranken.index', $drank->categorie) }}">{{ $drank->categorie }}</a>
             @endif
